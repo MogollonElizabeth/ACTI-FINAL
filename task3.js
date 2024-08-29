f9ea4a43b370a47c39485c2b0ab14cf4e403ffad
MogollonElizabeth <mogollonbuitragoanaelizabeth@gmail.com>
2024-08-28 23:36:38 -0500

    Update task3.js

--- task3.js
+++ task3.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const guardarURL = 'http://localhost:3000/users';
 
 export async function addUser(first_name, last_name, email) {
-    // Crear un objeto con los datos del usuario
+    
     const user = {
         first_name,
         last_name,
@@ -11,12 +11,12 @@ export async function addUser(first_name, last_name, email) {
     };
 
     try {
-        // Enviar el objeto como datos en el POST request
+       
         const response = await axios.post(guardarURL, user);
         return response.data;
     } catch (error) {
         console.error('Error adding user:', error);
-        // Puedes devolver un valor de error o manejar el error de otra manera aquí
+        
         return null;
     }
 }
