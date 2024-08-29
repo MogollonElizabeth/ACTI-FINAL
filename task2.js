c1d87e7d2fc1232a36989119ca0cfa692e4d119e
MogollonElizabeth <mogollonbuitragoanaelizabeth@gmail.com>
2024-08-28 23:24:11 -0500

    Añadidos archivos iniciales

---
+++ task2.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const SERVER_URL = 'http://localhost:3000/users';
+
+export async function listUsers() {
+    try {
+        const response = await axios.get(SERVER_URL);
+        const users = response.data;
+
+        // Convertir los datos a una lista de cadenas en el formato esperado
+        let formattedData = users.map(user => {
+            return `{` +
+                `\n  id: ${user.id},` +
+                `\n  first_name: '${user.first_name}',` +
+                `\n  last_name: '${user.last_name}',` +
+                `\n  email: '${user.email}'` +
+                `\n},`;
+        });
+
+        // Eliminar la última coma y añadir los corchetes de apertura y cierre
+        
+        formattedData = `[\n${formattedData.join('\n')}\n]`;
+
+        console.log(formattedData);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+    }
+}
