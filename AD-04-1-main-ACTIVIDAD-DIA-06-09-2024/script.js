826f33125b47d86b754adc83837c5fe079f904bd
MogollonElizabeth <mogollonbuitragoanaelizabeth@gmail.com>
2024-09-06 11:02:10 -0500

    ACTIVIDADDIA0609

---
+++ AD-04-1-main-ACTIVIDAD-DIA-06-09-2024/script.js
@@ -0,0 +1,26 @@
+// Make
+
+function comer(){
+
+    alert("ITS BURGERTIME")
+
+
+}
+
+function alguien(){
+
+    console.log("Someone click on BURGER TOWN!")
+
+
+}
+
+function color(){
+
+    const guarda= document.getElementById("ccolor");
+    guarda.style.color="red";
+
+
+}
+
+
+
